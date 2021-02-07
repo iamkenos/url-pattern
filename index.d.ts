4d75b34181c7ce75ebc56d73e84616c82c503ac5
iamkenos <alexis.matunog@gmail.com>
2021-02-07 10:55:40 +0800

    feat: refactored type defs to expose URLPatternOptions

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,24 @@
-interface UrlPatternOptions {
-    escapeChar?: string;
-    segmentNameStartChar?: string;
-    segmentValueCharset?: string;
-    segmentNameCharset?: string;
-    optionalSegmentStartChar?: string;
-    optionalSegmentEndChar?: string;
-    wildcardChar?: string;
+interface IUrlPatternOptions {
+  escapeChar?: string;
+  segmentNameStartChar?: string;
+  segmentValueCharset?: string;
+  segmentNameCharset?: string;
+  optionalSegmentStartChar?: string;
+  optionalSegmentEndChar?: string;
+  wildcardChar?: string;
 }
 
-declare class UrlPattern {
-    constructor(pattern: string, options?: UrlPatternOptions);
+declare module "url-pattern" {
+  namespace UrlPattern {
+    export type UrlPatternOptions = IUrlPatternOptions;
+  }
+  class UrlPattern {
+    constructor(pattern: string, options?: IUrlPatternOptions);
     constructor(pattern: RegExp, groupNames?: string[]);
 
     match(url: string): any;
     stringify(values?: any): string;
+  }
+
+  export = UrlPattern;
 }
-
-declare module UrlPattern { }
-
-export = UrlPattern;
